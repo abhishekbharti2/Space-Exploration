33bfaaa0fe32512eef5cde37f4178a920cc0079d
Abhishek Bharti <148630518+abhishekbharti2@users.noreply.github.com>
2024-11-23 22:18:20 +0000

    new 404

--- mainApp/src/components/Search.js
+++ mainApp/src/components/Search.js
@@ -1,5 +1,5 @@
 import DummyData from '../DataSet/DocsData.json'
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './Search.css';
 import './Navbar.js'
 
@@ -8,27 +8,38 @@ export default function Search(props) {
   const [result, setResult] = useState(DummyData);
   const [error, setError] = useState(null);
 
-    const getDataNow = async () =>{
+  const getDataNow = async () => {
     console.log(props.getData)
     let res = await fetch(`https://en.wikipedia.org/api/rest_v1/page/summary/${props.getData}`)
-    if(res.ok){
+    if (res.ok) {
       let data = await res.json();
       setResult(data);
       setError(null)
     }
-    else{
+    else {
       setError("Couldn't get the Data");
       setResult(null);
     }
   }
-  if(props.getData){
+  if (props.getData) {
     getDataNow();
     props.setData(null);
   }
   return (
     <div className="search-container">
-      {error && <img src = 'https://www.cedarforestfence.com/wp-content/uploads/2024/07/pngtree-error-404-page-not-found-png-image_3832696-removebg-preview.png' className='error-img' alt='error'/>}
-      
+      {error &&
+        <div class="error-container">
+        <h1> 404 </h1>
+        <p>
+            Oops! The page you're
+            looking for is not here.
+        </p>
+        <a href="/">
+            Go Back
+        </a>
+    </div>
+      }
+
       {result && (
         <div className="item-container">
           <h3 className="search-title">{result.title}</h3>
